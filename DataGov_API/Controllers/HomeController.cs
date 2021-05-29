19f78eebdcd2da7e8361f918e6797a8748f47636
wolfdogson <84239370+wolfdogson@users.noreply.github.com>
2021-05-29 18:20:57 -0400

    * .gitignore: * DataGov_API.sln: * Startup.cs: * Program.cs: * Parks.cs: * appsettings.json: * Error.cshtml: * Index.cshtml: * site.js: * favicon.ico: * site.css: * Privacy.cshtml: * Index.cshtml.cs: * Error.cshtml.cs: * Privacy.cshtml.cs: * Index.cshtml: * _ViewStart.cshtml: * _ViewStart.cshtml: * _ViewImports.cshtml: * _ViewImports.cshtml: * _Layout.cshtml: * _Layout.cshtml: * appsettings.Development.json: * LICENSE: * HomeController.cs: * LICENSE.txt: * launchSettings.json: * jquery.js: * jquery.min.js: * jquery.min.map: * LICENSE.md: * bootstrap.js: * bootstrap.css: * _ValidationScriptsPartial.cshtml: * bootstrap.js.map: * bootstrap.min.js: * bootstrap.css.map: * bootstrap.min.css: * bootstrap-grid.css: * bootstrap.bundle.js: * bootstrap.min.js.map: * bootstrap-reboot.css: * bootstrap.min.css.map: * jquery.validate.js: * LICENSE.txt: * bootstrap-grid.min.css: * bootstrap.bundle.min.js: * bootstrap.bundle.js.map: * bootstrap-grid.css.map: * bootstrap-reboot.min.css: * bootstrap-reboot.css.map: * additional-methods.js: * bootstrap.bundle.min.js.map: * jquery.validate.min.js: * bootstrap-grid.min.css.map: * bootstrap-reboot.min.css.map: * additional-methods.min.js: * jquery.validate.unobtrusive.js: * jquery.validate.unobtrusive.min.js:
    * DataGov_API.csproj: Api Pull Assignment

---
+++ DataGov_API/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DataGov_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DataGov_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        HttpClient httpClient;
+
+        static string BASE_URL = "https:/developer.nps.gov/api/v1/";
+        static string API_KEY = "WV8cfbS6mAsuSNquG7dRPwzVdgrwO0BkSZDC2qgF"; //Add your API key here inside ""
+        public IActionResult Index()
+        {
+
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string parksData = "";
+            Parks parks = null;
+
+
+            httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+                if (response.IsSuccessStatusCode)
+                {
+                    parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!parksData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    parks = JsonConvert.DeserializeObject<Parks>(parksData);
+                }
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(parks);
+        }
+    }
+}
+
---
+++ DataGov_API/Models/Parks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGov_API.Models
+{
+
+    public class Parks
+    {
+    public Park[] data { get; set; }
+    }
+
+    public class Park
+    {
+    public string parkCode { get; set; }
+    }
+}
+
